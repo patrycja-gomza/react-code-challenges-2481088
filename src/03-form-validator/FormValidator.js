1e48b128b5be74a3a8b2dc5218addf22ff2841a0
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-02-17 19:49:32 +0100

    validate a user sign-up form

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,12 +1,36 @@
 import { useState } from 'react'
 
-export default function FormValidator () {
+export default function FormValidator() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const errors = handleErrors()
+    setMessage(errors.length ? errors.join(', ') : 'User created!')
+  };
+
+  const handleErrors = () => {
+    const errors = [];
+    if (!email || !password || !passwordConfirm)
+      errors.push('All fields are required');
+
+    if (!email.includes('@') && !email.length !== 1) {
+      errors.push('Email must contain @');
+    }
+    if (password.length < 8) {
+      errors.push('Password must be at least 8 characters');
+    }
+    if (password !== passwordConfirm) {
+      errors.push('Passwords must match');
+    }
+    return errors;
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
@@ -23,6 +47,7 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
+      {message}
       <input type='submit' value='Submit' />
     </form>
   )
