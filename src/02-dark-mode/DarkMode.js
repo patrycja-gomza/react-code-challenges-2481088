86d59d0787d389abd85ff77a6de7ba1b77dbf023
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-02-17 18:13:03 +0100

    add state and toggle functionality to DarkMode component

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,26 @@
-export default function DarkMode () {
+import { useState } from 'react';
+
+export default function DarkMode() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={`page ${isDarkMode ? 'dark-mode' : ''}`}>
+      <button
+        className='dark-mode-button'
+        onClick={toggleMode}
+      >
+        Dark Mode
+      </button>
+      <button
+        className='light-mode-button'
+        onClick={toggleMode}
+      >
+        Light Mode
+      </button>
     </div>
   )
 }
