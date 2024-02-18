a65f2e099e4fe7a86f6743671d7d1eabb6a1bfdb
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-02-18 12:09:54 +0100

    add and remove effects within React

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,20 @@
-export default function WindowEvent () {
+import { useEffect } from "react"
+
+export default function WindowEvent() {
+  useEffect(() => {
+    const doubleClick = () => {
+      alert('mouse pressed!')
+    }
+
+    //add event listener to window that triggers an alert when the user double clicks
+    window.addEventListener('dblclick', doubleClick)
+
+    //remove event listener when component is unmounted
+    return () => {
+      window.removeEventListener('dblclick', doubleClick)
+    }
+  }, [])
+
   return (
     <h2>Window event active</h2>
   )
