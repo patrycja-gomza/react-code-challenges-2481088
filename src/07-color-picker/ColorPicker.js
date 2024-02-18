dcc81ecb3c4c4d78c6b4607359de21bb831ac5f0
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-02-18 12:33:47 +0100

    create a color picking app so the user can select the page's background color

--- src/07-color-picker/ColorPicker.js
+++ src/07-color-picker/ColorPicker.js
@@ -18,14 +18,19 @@ const colors = [{
   name: 'Tart Orange'
 }]
 
-export default function ColorPicker () {
+export default function ColorPicker() {
   const [backgroundColor, setBackgroundColor] = useState('white')
 
   return (
     <div className='page' style={{ backgroundColor }}>
       {
         colors.map(color => (
-          <Color key={color.hex} hex={color.hex} name={color.name} />
+          <Color
+            key={color.hex}
+            hex={color.hex}
+            name={color.name}
+            setBackgroundColor={setBackgroundColor}
+          />
         ))
       }
     </div>
