d98c8107c65bab6b65ff14345286361a5782f4d3
Patrycja Gomza <patrycja.gomza@gmail.com>
2024-02-18 11:26:41 +0100

    use a dog picture API to display dog images on react

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,31 @@
-export default function DogPics () {
+import { useEffect, useState } from "react";
+
+const getDogPic = async () => {
+  const response = await fetch('https://dog.ceo/api/breeds/image/random')
+  const data = await response.json()
+  return data.message
+};
+
+export default function DogPics() {
   // API: https://dog.ceo/dog-api/
+
+  const [dogPic, setDogPic] = useState('');
+
+  useEffect(() => {
+    getDogPic().then((pic) => setDogPic(pic));
+  }, []);
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img src={dogPic} />
+      <button
+        onClick={async () => {
+          const pic = await getDogPic();
+          setDogPic(pic);
+        }}
+      >
+        🐶
+      </button>
     </div>
   )
 }
